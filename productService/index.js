40cf6346d304ea401a61cc3210c0b75c5cf66866
Diyouf <diyoufkv7@gmail.com>
2024-03-26 15:36:18 +0530

    product(Service):Create product completed

---
+++ productService/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const Product = require('./productModel')
+
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/MicroservicesProducts')
+    .then(() => {
+        console.log('mongodb conneected successfully')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+
+
+app.post('/product/create', async (req, res) => {
+    const {name , price , description} = req.body
+    const newProduct = new Product({
+        name,
+        description,
+        price
+    })
+    await newProduct.save()
+    res.json(newProduct)
+})
+
+
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('MongoDB connection closed');
+        process.exit(0);
+    });
+});
+
+
+app.listen(3002, () => {
+    console.log('Product Service running at 3002')
+})
